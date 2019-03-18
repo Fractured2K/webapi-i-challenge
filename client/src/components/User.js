400bfabf2823398a42ba1fece53a2ac3a7369387
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-18 14:38:33 -0700

    Implemented Font Awesome

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,11 +1,25 @@
 import React, { Fragment } from "react";
 
+// Font awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrashAlt, faPencilAlt);
+
 const User = props => {
 	return (
 		<div className="user">
 			<h1>{props.name}</h1>
 			<p>{props.bio}</p>
-			<button className="delete">Delete</button>
+
+			<button className="update">
+				<FontAwesomeIcon icon={faPencilAlt} style={{ color: "#fff" }} />
+			</button>
+
+			<button className="delete">
+				<FontAwesomeIcon icon={faTrashAlt} style={{ color: "#fff" }} />
+			</button>
 		</div>
 	);
 };
