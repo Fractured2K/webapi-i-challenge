0c6911cf1d1fac9526321891cf43a400b00b9e5f
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-18 12:03:45 -0700

    Get user by id endpoint functionality

--- index.js
+++ index.js
@@ -48,7 +48,24 @@ server.get("/api/users", (req, res) => {
 });
 
 // Get user by id endpoint
-server.get("/api/users/:id", (req, res) => {});
+server.get("/api/users/:id", (req, res) => {
+	const { id } = req.params;
+
+	db.findById(id)
+		.then(user => {
+			if (!user)
+				return res.status(404).json({
+					message: "The user with the specified ID does not exist."
+				});
+
+			return res.status(200).json(user);
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: "The user information could not be retrieved."
+			});
+		});
+});
 
 server.listen(3001, () => {
 	console.log(`=== Server now listening on http://localhost:3001 ===`);
