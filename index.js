ffc36d1f9b7869642dfd1a34578ae1e127f8a7fe
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-18 11:33:54 -0700

    Create user endpoint error handling

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const db = require("./data/db");
 
 const server = express();
 
-// Create user route
-server.post("/api/users", (req, res) => {});
+// Create user endpoint
+server.post("/api/users", (req, res) => {
+	const user = req.body;
+
+	if (!user.name || !user.body)
+		return res
+			.status(404)
+			.json({
+				errorMessage: "Please provide name and bio for the user."
+			})
+			.end();
+});
 
 server.listen(3001, () => {
 	console.log(`=== Server now listening on http://localhost:3001 ===`);
