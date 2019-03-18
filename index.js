97d1cd555143920a09602134eab0f3427beb1504
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-18 11:22:37 -0700

    Scaffold API

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// implement your API here
+const express = require("express");
+
+const server = express();
+
+server.listen(3001, () => {
+	console.log(`=== Server now listening on http://localhost:3001 ===`);
+});
