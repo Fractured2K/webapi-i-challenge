9c055a42aa1bf7ec4c42a5f0ae4643d83259ef09
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-18 13:00:34 -0700

    Fetch users on component mount with useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-	const [users, setUser] = useState();
+	const [users, setUser] = useState({ users: [] });
+
+	useEffect(async () => {
+		const fetchUsers = async () => {
+			const res = await axios("http://localhost:3001/api/users");
+
+			setUser(res.data);
+		};
+
+		fetchUsers();
+	}, []);
+
+	return (
+		<div>
+			<h1>{console.log(users)}</h1>
+		</div>
+	);
 }
+
+export default App;
