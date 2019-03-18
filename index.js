f1decc3aa16d37679f5c183aa66fe0be71422f2c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-18 11:28:23 -0700

    Define create user endpoint

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const db = require("./data/db");
 
 const server = express();
 
+// Create user route
+server.post("/api/users", (req, res) => {});
+
 server.listen(3001, () => {
 	console.log(`=== Server now listening on http://localhost:3001 ===`);
 });
